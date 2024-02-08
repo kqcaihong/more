a87c0d24a6e780fae9dccfd8d7b77994c7b2bba2
侯凯旗 <alan.hou@pkfare.com>
2024-02-08 14:56:31 +0800

    feat: RestTemplate测试

--- src/main/java/com/learn/more/MoreApplication.java
+++ src/main/java/com/learn/more/MoreApplication.java
@@ -1,9 +1,11 @@
 package com.learn.more;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,13 +17,25 @@ public class MoreApplication {
   }
 
   @Bean
-  public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
+  public RestTemplate restTemplate(@Qualifier("okHttp3ClientHttpRequestFactory") ClientHttpRequestFactory factory) {
     return new RestTemplate(factory);
   }
 
   @Bean
-  public ClientHttpRequestFactory clientHttpRequestFactory() {
+  public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
     SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+    // HttpURLConnection只支持设置这两个超时时间
+    factory.setConnectTimeout(10000);
+    factory.setReadTimeout(30000);
+    return factory;
+  }
+
+  @Bean
+  public ClientHttpRequestFactory okHttp3ClientHttpRequestFactory() {
+    OkHttp3ClientHttpRequestFactory factory = new OkHttp3ClientHttpRequestFactory();
+    factory.setConnectTimeout(10000);
+    factory.setReadTimeout(30000);
+    factory.setWriteTimeout(30000);
     return factory;
   }
 }
