f433e65cfa194a3faadfb7235b2d3479a03cb6f6
侯凯旗 <alan.hou@pkfare.com>
2024-02-21 20:50:33 +0800

    feat: 完善代码

--- src/main/java/com/learn/more/httpclient/OkHttp3ClientTest.java
+++ src/main/java/com/learn/more/httpclient/OkHttp3ClientTest.java
@@ -1,5 +1,10 @@
 package com.learn.more.httpclient;
 
+import java.io.IOException;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -7,19 +12,17 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
-import java.io.IOException;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
-
 public class OkHttp3ClientTest {
 
   public static void main(String[] args) throws IOException {
+    doAsyncGet("http://localhost:8010/user/queryAll");
+
     String param = "{\"name\":\"Andy\",\"age\":18}";
-    String result = doPostByOkhttp3("http://localhost:8010/user/add", param);
+    String result = doPost("http://localhost:8010/user/add", param);
     System.out.println(result);
   }
 
-  private static String doPostByOkhttp3(String url, String param) throws IOException {
+  private static String doPost(String url, String param) throws IOException {
     OkHttpClient client = new OkHttpClient.Builder()
         .connectTimeout(10L, TimeUnit.SECONDS)
         .readTimeout(30L, TimeUnit.SECONDS)
@@ -37,5 +40,30 @@ public class OkHttp3ClientTest {
       }
       return "";
     }
+  }
+
+  private static void doAsyncGet(String url) {
+    OkHttpClient client = new OkHttpClient.Builder()
+        .connectTimeout(10L, TimeUnit.SECONDS)
+        .readTimeout(30L, TimeUnit.SECONDS)
+        .build();
+    Request request = new Request.Builder()
+        .url(url)
+        .get()
+        .addHeader("Accept-Type", "application/json")
+        .build();
+
+    client.newCall(request).enqueue(new Callback() {
+      @Override
+      public void onFailure(Call call, IOException e) {
+
+      }
+
+      @Override
+      public void onResponse(Call call, Response response) throws IOException {
+        assert response.body() != null;
+        System.out.println(response.body().string());
+      }
+    });
   }
 }
